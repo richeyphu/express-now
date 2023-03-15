0816e50aeab45bec6e21356fb6bf48f856d8554a
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-03-16 05:01:41 +0700

    Update test

--- tests/status.test.ts
+++ tests/status.test.ts
@@ -4,13 +4,14 @@ import supertest, { SuperTest, Test } from 'supertest';
 import { serverStatus } from '../dist';
 
 const app: Express = express();
-app.use('/status', serverStatus(app));
+const route = '/status';
+app.use(route, serverStatus(app));
 
 const request: SuperTest<Test> = supertest(app);
 
 describe('serverStatus', () => {
   it('should response the GET method', async () => {
-    const response = await request.get('/status');
+    const response = await request.get(route);
     try {
       expect(response.statusCode).toBe(200);
     } catch (error) {
@@ -19,7 +20,7 @@ describe('serverStatus', () => {
   });
 
   it('should response the `server.status` as `up`', async () => {
-    const response = await request.get('/status');
+    const response = await request.get(route);
     try {
       expect(response.body.server.status).toBe('up');
     } catch (error) {
@@ -27,10 +28,32 @@ describe('serverStatus', () => {
     }
   });
 
-  it('should response the `server.env as `test`', async () => {
-    const response = await request.get('/status');
+  it('should response the `server.env` as `test`', async () => {
+    const response = await request.get(route);
     try {
       expect(response.body.server.env).toBe('test');
+    } catch (error) {
+      expect(response.statusCode).toBe(401);
+    }
+  });
+
+  it('should response the `server.name` as in `package.json`', async () => {
+    const response = await request.get(route);
+    const packageJson = await import('../package.json');
+    const serverName = packageJson.name;
+    try {
+      expect(response.body.server.name).toBe(serverName);
+    } catch (error) {
+      expect(response.statusCode).toBe(401);
+    }
+  });
+
+  it('should response the `server.version` as in `package.json`', async () => {
+    const response = await request.get(route);
+    const packageJson = await import('../package.json');
+    const serverVersion = packageJson.version;
+    try {
+      expect(response.body.server.version).toBe(serverVersion);
     } catch (error) {
       expect(response.statusCode).toBe(401);
     }
